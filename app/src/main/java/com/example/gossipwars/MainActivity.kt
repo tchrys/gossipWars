9cdf023b04b5a91043759e168be58ba490dd40b4
tchrys <tanasescuchrys@gmail.com>
2020-10-22 20:02:59 +0300

    Bottom navigation second activity created.

--- app/src/main/java/com/example/gossipwars/MainActivity.kt
+++ app/src/main/java/com/example/gossipwars/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.gossipwars
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, InGameActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
